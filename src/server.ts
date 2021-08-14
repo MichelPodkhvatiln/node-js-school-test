ca907fb3b6808ab748df47730ac1b14133587ee2
MichelPodkhvatiln <podkhvatilin.work@gmail.com>
2021-08-14 22:56:36 +0300

    add stormdb for project

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,23 @@
-import * as http from 'http'
+import http from 'http'
+import { join } from 'path'
+import StormDB from 'stormdb'
 import { getRequestData } from "./helpers"
 
-const db = {
-    users: []
-}
+const engine = new StormDB.localFileEngine(join(__dirname, './db/db.stormdb'))
+const db = new StormDB(engine)
+
+db.default({ users: [] })
 
 const server = http.createServer( async (req, res) => {
     if (req.url === '/create-user' && req.method === 'POST') {
         const rawData = await getRequestData(req)
         const data = JSON.parse(rawData)
-        console.log(data)
 
-        // @ts-ignore
-        db.users.push(data)
-
-        console.log(db)
+        db.get('users').push(data)
+        db.save()
 
         res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify({ db }))
+        res.end(JSON.stringify({ db: db.get('users').value() }))
     }
     else if (req.url?.match(/\/user\/([\d]+)/) && req.method === 'GET') {
         const id = req.url.split('/')[2]
@@ -31,7 +31,7 @@ const server = http.createServer( async (req, res) => {
     }
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3030
 
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}.`)
